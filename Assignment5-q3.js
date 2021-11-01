e8e078a3d71c3e2cf67116de38d8ef499513275f
c-vani <90023177+c-vani@users.noreply.github.com>
2021-11-01 21:21:23 +0530

    Add files via upload

---
+++ Assignment5-q3.js
@@ -0,0 +1,15 @@
+var k;//variable 'k' has only been declared not initialized,hence it is UNDEFINED
+if(k==undefined)
+{
+    console.log("k is undefined");
+}
+k=5;//Now variable 'k' has been initialized,hence it is DEFINED
+if(k==undefined)
+{
+    console.log("k is undefined");
+}
+else
+{
+    console.log("k has been defined");
+}
+console.log(b);//variable 'b' has not been even declared,hence it is NOT DEFINED
