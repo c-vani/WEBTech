319e424f28645480703523c443f9c6f2c74564c2
c-vani <90023177+c-vani@users.noreply.github.com>
2021-11-26 18:08:48 +0530

    Add files via upload

---
+++ Assignment-6_js.js
@@ -0,0 +1,18 @@
+var Person1={
+    FirstName:"Arshi",
+    LastName:"Singh",
+    OfficeAddress:"36,new palasia,Indore"
+};
+var Person2={
+    FirstName:"Ritu",
+    LastName:"Singhaniya",
+    OfficeAddress:"78, ring road,Pune"
+};
+var Person3={
+    FirstName:"Mahi",
+    LastName:"Arora",
+    OfficeAddress:"06,morya building,Delhi"
+};
+console.log(Person1);
+console.log(Person2);
+console.log(Person3);
